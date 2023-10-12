724ad34c18a4a0400ff422e5ebfd3c6c11f924ce
Casey Henderson <casey.henderson@k-int.co.uk>
2023-10-12 16:16:07 +0100

    fix: Fix access token issues [DCB-488]

--- dcb-hub-admin-ui/src/components/Alert/Alert.tsx
+++ dcb-hub-admin-ui/src/components/Alert/Alert.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Alert as MUIAlert, AlertTitle} from '@mui/material';
+import { capitalize } from '@mui/material/utils';
 
 export default function Alert(props: any) {
     return(
         <>
             <MUIAlert severity={props.severityType} onClose={props.onCloseFunc}>
-                <AlertTitle sx={{style: {textTransform: 'capitalize'}}}>{props.severityType}</AlertTitle>
+                <AlertTitle>{capitalize(props.severityType)}</AlertTitle>
                 {props.alertText}
             </MUIAlert>
         </>
