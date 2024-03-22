bfbaad4d6f5877f33fdde207639c64b67ffb6e67
Casey Henderson <casey.henderson@k-int.co.uk>
2024-03-22 16:41:53 +0000

    test: Implement colour checking in Cypress
    Cypress insists on having colours in RGB format, and the usual
    conversion libraries they recommend  weren't working for Cypress, so I
    did it myself.
    Usage example in locations.cy.ts.
    Should also work for RGBA values if you supply an alpha.

--- dcb-hub-admin-ui/cypress/e2e/locations.cy.ts
+++ dcb-hub-admin-ui/cypress/e2e/locations.cy.ts
@@ -1,24 +1,37 @@
-import { basicUser } from "../users/basic_user"
-describe('Locations page', () => {
-    beforeEach(() => {
-      cy.login(basicUser)
-        cy.visit('http://localhost:3000/locations')
-        cy.intercept('POST', '/graphql', { fixture: 'locations.json'}).as('loadLocations')
-    })    
-    it('should render the locations page with the correct data and UI elements', () => {
-      cy.get('[id=page-title]').should('have.text', 'Locations');
-      cy.get('[data-id=01cd1cd7-7c67-5ca3-b0f4-4d709792a804]').should('contain', 'Concordia Seminary');
-    })
+import { basicUser } from "../users/basic_user";
+import hexToRgb from "../utils/hex-to-rgb";
+describe("Locations page", () => {
+  beforeEach(() => {
+    cy.login(basicUser);
+    cy.visit("http://localhost:3000/locations");
+    cy.intercept("POST", "/graphql", { fixture: "locations.json" }).as(
+      "loadLocations"
+    );
+  });
+  it("should render the locations page with the correct data and UI elements", () => {
+    // A test example of checking colours in Cypress
+    const yourColor = hexToRgb("#0C4068");
+    cy.get("[id=page-title]")
+      .should("have.text", "Locations")
+      .should("have.css", "color", yourColor);
+    cy.get("[data-id=01cd1cd7-7c67-5ca3-b0f4-4d709792a804]").should(
+      "contain",
+      "Concordia Seminary"
+    );
+  });
 
-    it('should search for a location and return the expected result', () => {
-        // Conduct an example search, and verify the result is as we expect
-        cy.get('[aria-label=Search]').type('Paradise Street');
-        // 'PS' is the location code for the Paradise Street location - see locations.json
-        // This is verifying that not only does the location have the name we expect, 
-        // but that it also has all of thethe other data we would expect it to have.
-        cy.get('[data-id=7250910d-1b73-59ac-8482-8b53d0eb12b8]').should('contain', 'ps')
-    })
-    afterEach(() => {
-      cy.logout();
-    })
-  })
+  it("should search for a location and return the expected result", () => {
+    // Conduct an example search, and verify the result is as we expect
+    cy.get("[aria-label=Search]").type("Paradise Street");
+    // 'PS' is the location code for the Paradise Street location - see locations.json
+    // This is verifying that not only does the location have the name we expect,
+    // but that it also has all of thethe other data we would expect it to have.
+    cy.get("[data-id=7250910d-1b73-59ac-8482-8b53d0eb12b8]").should(
+      "contain",
+      "ps"
+    );
+  });
+  afterEach(() => {
+    cy.logout();
+  });
+});
---
+++ dcb-hub-admin-ui/cypress/utils/hex-to-rgb.ts
@@ -0,0 +1,40 @@
+/**
+ * Converts a hex value into either an RGB or a RGBA value.
+ * This should make our lives easier when checking colour values in Cypress, which only accepts rgb values.
+ *
+ * To get an RGBA value, you must supply a valid alpha value.
+ *
+ * @param string The hex value you'd like to convert. ('423456'. '#423456', ''424', '#424')
+ * @param number An alpha value. (optional) ('0.2', '0.5')
+ * @return An rgb or rgba value. ('rgb(26, 42, 33)'. 'rgba(26, 43, 44, 0.5)')
+ */
+
+export default function hexToRgb(hex: string, alpha?: number): string | null {
+  // First, remove the hash
+  hex = hex.replace("#", "");
+
+  // Next, check if the hex value is valid
+  // TODO: replace or simplify over-complicated regex
+  if (!/^[A-Fa-f0-9]{6}$/i.test(hex)) {
+    return null;
+  }
+
+  // Now we need to parse the hex value into RGB components
+  const hexInt = parseInt(hex, 16); // This holds the integer value of the hex code.
+  // We can then use mathematical operations to get what we want
+  const r = (hexInt >> 16) & 255; // right shift by 16 bits for the red
+  const g = (hexInt >> 8) & 255; // right shift again (8 bits) to get the green
+  const b = hexInt & 255; // mask the lowest 8 bits to get the blue
+
+  // If alpha value is provided, return RGBA
+  if (typeof alpha === "number") {
+    if (alpha < 0 || alpha > 1) {
+      throw new Error("Invalid alpha value. Alpha must be between 0 and 1.");
+    }
+    // Else the alpha is valid, so add it
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  }
+
+  // Otherwise, return RGB
+  return `rgb(${r}, ${g}, ${b})`;
+}
