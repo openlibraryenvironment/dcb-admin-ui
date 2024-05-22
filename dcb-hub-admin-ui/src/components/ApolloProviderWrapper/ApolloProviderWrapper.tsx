a5a3dc146c96887c065b3a197e466f7303045ac6
Casey Henderson <casey.henderson@k-int.co.uk>
2024-05-22 10:21:23 +0100

    fix: Fix initial request failures [DCB-1149]
    Forces Apollo Client to wait for a session to load to initialise, and
    supplies a valid token to the initial request from Apollo, eliminating
    the 401 seen on first load in DCB Admin.

--- dcb-hub-admin-ui/src/components/ApolloProviderWrapper/ApolloProviderWrapper.tsx
+++ dcb-hub-admin-ui/src/components/ApolloProviderWrapper/ApolloProviderWrapper.tsx
@@ -11,11 +11,19 @@ import { useSession } from "next-auth/react";
 import getConfig from "next/config";
 
 export const ApolloProviderWrapper = ({ children }: PropsWithChildren) => {
-	const { data: session } = useSession();
+	const { data: session, status } = useSession();
+	const isSessionLoading = status === "loading";
+	const token = session?.accessToken;
+
 	const url = useMemo(() => {
 		const { publicRuntimeConfig } = getConfig();
-		return new HttpLink({ uri: publicRuntimeConfig.DCB_API_BASE + "/graphql" });
-	}, []);
+		return new HttpLink({
+			uri: publicRuntimeConfig.DCB_API_BASE + "/graphql",
+			headers: {
+				authorization: token ? `Bearer ${token}` : "",
+			},
+		});
+	}, [token]);
 
 	const client = useMemo(() => {
 		const authMiddleware = setContext((operation, { headers }) => {
@@ -34,6 +42,12 @@ export const ApolloProviderWrapper = ({ children }: PropsWithChildren) => {
 			ssrMode: true,
 		});
 	}, [url, session?.accessToken]);
+
+	// Wait to initialise the client until the session has loaded.
+	// This means nothing will be rendered until then.
+	if (isSessionLoading) {
+		return null;
+	}
 
 	return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
