126b04763bf9c928b64e4050fb7b8a05f99b72ff
Casey Henderson <casey.henderson@k-int.co.uk>
2024-08-29 14:46:20 +0000

    feat: Add "does not equal" and "does not contain" filters [DCB-1406] [DCB-1409]

--- dcb-hub-admin-ui/src/helpers/filters.ts
+++ dcb-hub-admin-ui/src/helpers/filters.ts
@@ -1,16 +1,63 @@
-import { getGridStringOperators } from "@mui/x-data-grid-pro";
+import {
+	getGridStringOperators,
+	GridCellParams,
+	GridFilterInputValue,
+	GridFilterOperator,
+} from "@mui/x-data-grid-pro";
+// Bear in mind there are also numeric operators for those columns.
 
-export const standardFilters = getGridStringOperators().filter(({ value }) =>
-	[
-		"equals",
-		"contains" /* add more over time as we build in support for them */,
-	].includes(value),
-);
+// The logic for these custom filters is actually contained in the ServerPaginatedGrid for most cases
+// buildQuery method - as they must translate queries into Lucene syntax to be processed on the server
+// As such these exist for two reasons: one to actually get these filter options presented to the user
+// and two - to filter in situations where the ClientDataGrid is used.
 
-export const equalsOnly = getGridStringOperators().filter(({ value }) =>
-	["equals"].includes(value),
-);
+const doesNotEqualFilter: GridFilterOperator = {
+	label: "does not equal",
+	value: "does not equal",
+	getApplyFilterFn: (filterItem) => {
+		if (!filterItem.field || !filterItem.value || !filterItem.operator) {
+			return null;
+		}
+		return (params) => params[filterItem.field] !== filterItem.value;
+	},
+	InputComponent: GridFilterInputValue,
+};
 
-export const containsOnly = getGridStringOperators().filter(({ value }) =>
-	["contains"].includes(value),
-);
+const doesNotContainFilter: GridFilterOperator = {
+	label: "does not contain",
+	value: "does not contain",
+	getApplyFilterFn: (filterItem) => {
+		if (!filterItem.field || !filterItem.value) {
+			return null;
+		}
+
+		return (params: GridCellParams) => {
+			const cellValue = params[filterItem.field as keyof GridCellParams];
+			if (typeof cellValue === "string") {
+				return !cellValue.includes(filterItem.value as string);
+			}
+			return false; // If cell value is not a string, the filter does not apply
+		};
+	},
+	InputComponent: GridFilterInputValue,
+};
+
+export const standardFilters = [
+	...getGridStringOperators().filter(({ value }) =>
+		["equals", "contains"].includes(value),
+	),
+	doesNotEqualFilter,
+	doesNotContainFilter,
+];
+
+export const equalsOnly = [
+	...getGridStringOperators().filter(({ value }) => ["equals"].includes(value)),
+	doesNotEqualFilter,
+];
+
+export const containsOnly = [
+	...getGridStringOperators().filter(({ value }) =>
+		["contains"].includes(value),
+	),
+	doesNotContainFilter,
+];
