40f3590898293abe0d999ca9b8f6843d39a90ff8
Casey Henderson <casey.henderson@k-int.co.uk>
2025-05-22 14:31:05 -0400

    fix: Fix staff request incorrect translation key [DCB-1555]

--- dcb-hub-admin-ui/src/helpers/getRequestError.ts
+++ dcb-hub-admin-ui/src/helpers/getRequestError.ts
@@ -6,6 +6,6 @@ export function getRequestError(failedChecks: FailedChecks[]): string {
 	if (failedChecks[0].code == "DUPLICATE_REQUEST_ATTEMPT") {
 		return "staff_request.patron.error.request.duplicate";
 	} else {
-		return "staff_request.patron.error.generic";
+		return "staff_request.patron.error.request.generic";
 	}
 }
