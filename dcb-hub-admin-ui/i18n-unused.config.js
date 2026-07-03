9b73b4a0d0a8bbd320785b071cf2db304d929a5c
Casey Henderson <casey.henderson@k-int.co.uk>
2026-07-03 13:25:05 +0100

    chore: Update to ESLint and i18n-unused config

--- dcb-hub-admin-ui/i18n-unused.config.js
+++ dcb-hub-admin-ui/i18n-unused.config.js
@@ -2,4 +2,7 @@
 module.exports = {
 	localesPath: "src/locales/en-GB",
 	srcPath: "src",
+	translationKeyMatcher: /(?:[$ .{\[](_|t|tc|i18nKey))\(.*?\)/gis,
 };
+// Gnarly regex in translation key matcher is intended to catch
+// the various presentations (such as multi-lines) of our translation keys and t functions
